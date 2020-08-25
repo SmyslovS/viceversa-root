6bbcce3387a632d31957d0d58abecbf67f67df5f
SmyslovS <smyslov.noed@gmail.com>
2020-08-25 19:07:58 +0300

    Created reverser

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,4 +6,11 @@ def get_home_page(request):
 
 
 def get_reverse_page(request):
-    return render(request, 'reverse.html')
+    user_text = request.GET['user-text']
+    reversed_text = user_text[::-1]
+    amount_of_words = len(user_text.split())
+    return render(request, 'reverse.html',
+                  {'amount_of_words': amount_of_words,
+                   'user_text': user_text,
+                   'reversed_text': reversed_text,
+                   })
