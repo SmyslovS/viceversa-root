e02973469bc7ed564d865810a1829eb14e41093c
SmyslovS <smyslov.noed@gmail.com>
2020-08-25 17:30:17 +0300

    Created reverse page

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_home_page),
+    path('reverse', views.get_reverse_page, name='reverse'),
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+
 
 def get_home_page(request):
     return render(request, 'home.html')
+
+
+def get_reverse_page(request):
+    return render(request, 'reverse.html')
